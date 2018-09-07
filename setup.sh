75b59a7028cc911f9295f67e71721343caf2042a
harris-miller <harrismillerconsulting@gmail.com>
2018-09-07 12:59:23 -0600

    updated brews

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # brews
-brew install cmatrix git heroku openssl postgresql python readline redis sqlite thefuck watchman zsh zsh-completions zsh-syntax-highlighting
+brew install cmatrix git heroku openssl postgresql python readline redis sqlite thefuck watchman zsh zsh-completions zsh-syntax-highlighting bat
 # casks
 brew cask install firefox flux google-chrome iterm2 itsycal onyx p4merge postman sourcetree spectacle spotify the-unarchiver visual-studio-code vlc
 
